db8edefd6044994bfc9e5b2e6a35a05d13502127
mouweng <325242850@qq.com>
2022-01-09 23:44:47 +0800

    my-blog init

---
+++ my-blog/README.md
@@ -0,0 +1,36 @@
+# my-blog
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+
+## 学习过程记录
+```shell
+npm install --global vue-cli
+vue init web my-blog # 全部选no
+cd my-blog
+npm install
+npm run dev
+```
+
+npm install vue-resource --save
+
+v-for出现问题解决方案：
+https://www.cnblogs.com/zdz8207/p/vue-for-v-bind-key.html
---
+++ my-blog/src/components/AddBlog.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="add-blog"> 
+    <h2>添加博客</h2>
+    <form v-if="!submitted"> 
+        <label>博客标题</label>
+        <input type="text" v-model="blog.title" required>
+        <label>博客内容</label>
+        <textarea v-model="blog.content"></textarea>
+
+        <div id = "checkboxes">
+            <label>Vue.js</label>
+            <input type = "checkbox" value="vue.js" v-model="blog.categories">
+            <label>Node.js</label>
+            <input type = "checkbox" value="Node.js" v-model="blog.categories">
+            <label>React.js</label>
+            <input type = "checkbox" value="React.js" v-model="blog.categories">
+            <label>Angular4</label>
+            <input type = "checkbox" value="Angular4" v-model="blog.categories">
+        </div>
+
+        <label>作者</label>
+        <select>
+            <option v-for="author in authors" :key="author">
+                {{author}}
+            </option>
+        </select>
+        <button v-on:click.prevent="post">添加博客</button>
+    </form>
+    <div v-if="submitted">
+        <h3>您的博客发布成功</h3>
+    </div>
+    <hr>
+    <div id="preview">
+        <h3>博客总览</h3>
+        <p>博客标题：{{blog.title}}</p>
+        <p>博客内容：</p>
+        <p>{{blog.content}}</p>
+        <p>博客分类：</p>
+        <ul>
+            <li v-for="category in blog.categories"  :key="category">
+                {{category}}
+            </li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'add-blog',
+    data() {
+        return {
+            blog: {
+                title: "",
+                content: "",
+                categories: [],
+            },
+            authors: ["wengyifan","wyf","mouweng"],
+            submitted: false
+        }
+    },
+    methods: {
+        post:function() {
+            // https://jsonplaceholder.typicode.com/posts
+            this.$http.post("https://jsonplaceholder.typicode.com/posts", {
+                title: this.blog.title,
+                body: this.blog.content,
+                userId:1
+            }).then(function(data){
+                console.log(data);
+                this.submitted = true;
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
